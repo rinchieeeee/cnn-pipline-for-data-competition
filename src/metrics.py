128747e258997185691e5a881ed8f4d456c23e56
rinchieeeee <hayashi5151@icloud.com>
2021-07-19 21:43:25 +0900

    add metrics for criterion

---
+++ src/metrics.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.metrics import roc_auc_score
+
+def get_score(y_true, y_pred):
+    scores = []
+    for i in range(y_true.shape[1]):
+        score = roc_auc_score(y_true[:,i], y_pred[:,i])
+        scores.append(score)
+    avg_score = np.mean(scores)
+    return avg_score, scores
