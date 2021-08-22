f281b0774d2a8b4c8e0236ec4f11c83c627d3cc3
rinchieeeee <hayashi5151@icloud.com>
2021-08-23 00:38:21 +0900

    fixed some issues to adapt new style

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,4 +1,4 @@
-from torch.optim import Adam, SGD
+from torch.optim import Adam, SGD, AdamW
 import yaml
 
 def get_optimizer(learning_params, config: dict):
@@ -13,5 +13,28 @@ def get_optimizer(learning_params, config: dict):
         return Adam(learning_params, **config_optim["params"])
 
 
+    if config_optim["name"] == "AdamW":
+        return AdamW(learning_params, **config_optim["params"])
+        
     if config_optim["name"] == "SGD":
-        return SGD()
+        return SGD(learning_params, **config_optim["params"])
+
+
+
+def get_optimizer_for_lr_find(learning_params, lr_init, config: dict):
+    """
+    learning_params: learnable parameter of model
+    config: original dict opning yaml file
+    """
+
+    config_optim = config["optimizer"]
+
+    if config_optim["name"] == "Adam":
+        return Adam(learning_params, lr = lr_init, **config_optim["params"])
+
+
+    if config_optim["name"] == "AdamW":
+        return AdamW(learning_params, lr = lr_init, **config_optim["params"])
+
+    if config_optim["name"] == "SGD":
+        return SGD(learning_params, lr = lr_init, **config_optim["params"])
